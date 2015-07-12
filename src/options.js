ea14d553c01a190ff4a91905fda87f8a8a8c7f8a
Jimmy Mullen <jimmy.mullen@gmail.com>
2015-07-12 13:17:18 -0400

    big refactor and cleanup

--- src/options.js
+++ src/options.js
@@ -1,100 +1,114 @@
 
 
+//---------- UI Functions -------------
 
 function insertPicker() {
   jQuery('#datetimepicker').datetimepicker();
 }
 
-function hash(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+function showStatus(msg) {
+  var status = document.getElementById('status');
+  status.textContent = msg;
+  setTimeout(function() {
+    status.textContent = '';
+  }, 1500);
 }
 
-function removeItem(item) {
+function clearTextFieldInput() {
+  var opt = document.getElementById('opt');
+  opt.value = "";
+  opt.focus();
+}
+
+function initializePage() {
+  document.addEventListener('DOMContentLoaded', restoreTabs);
+  document.addEventListener('DOMContentLoaded', insertPicker);
+  document.getElementById('save').addEventListener('click', saveTabs);
+  document.getElementById('opt').addEventListener('click', function() {this.select();});
+  document.onkeydown=function(){
+    if(window.event.keyCode=='13' && document.getElementById('opt').value != ""){
+      saveTabs();
+    }
+  }
+}
+
+//-------------------------------------
+
+//---------- Tab Management -----------
+
+function removeTab(tab) {
   chrome.storage.sync.get({
-    myList: []
+    myTabs: []
   }, function(items) {
-    var listFromStorage = items.myList;
-    var index = listFromStorage.indexOf(item);
+    var tabsFromStorage = items.myTabs;
+    var index = tabsFromStorage.indexOf(tab);
     if (index > -1) {
-      listFromStorage.splice(index, 1);
+      tabsFromStorage.splice(index, 1);
     }
     chrome.storage.sync.set({
-      myList: listFromStorage
+      myTabs: tabsFromStorage
     }, function() {
-      refreshList(listFromStorage);
-      var status = document.getElementById('status');
-      status.textContent = 'Tab Deleted.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 1500);
-  });
-});
-}
-
-function refreshList(newList) {
-  var ul = document.getElementById('ul');
-    ul.innerHTML = '';
-    for (var i = 0; i < newList.length; i++) {
-      var tab = newList[i];
-      var tabhash = hash(tab);
-      var listItem = document.createElement('li');
-      listItem.innerHTML = tab + " ";
-      listItem.setAttribute("id", tab);
-      var deleteText = document.createTextNode('[x]');
-      var deleteLink = document.createElement('a');
-      deleteLink.setAttribute('href', '#');
-      deleteLink.setAttribute('id', "del_" + tab);
-      deleteLink.appendChild(deleteText);
-      deleteLink.onclick = function () { removeItem(this.getAttribute('id').split('_')[1]); };
-      listItem.appendChild(deleteLink);
-      //item.appendChild(document.createTextNode(newList[i]));
-      //item.appendChild(document.createTextNode("<a href='#'>[x]</a>"));
-      ul.appendChild(listItem);
-    }
-  document.getElementById('list').appendChild(ul);
-}
-
-function save_options() {
-  var opt = document.getElementById('opt').value;
-  chrome.storage.sync.get({
-    myList: []
-  }, function(items) {
-    var listFromStorage = items.myList;
-    listFromStorage.push(opt);
-    chrome.storage.sync.set({
-      myList: listFromStorage
-    }, function() {
-      refreshList(listFromStorage);
-      var opt = document.getElementById('opt');
-      opt.value = "";
-      opt.focus();
-
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 1500);
+      buildList(tabsFromStorage);
+      showStatus('Tab Deleted');
     });
   });
 }
 
-function restore_options() {
+function buildListItem(tab) {
+  var listItem = document.createElement('li');
+  listItem.innerHTML = tab + " ";
+  listItem.setAttribute("id", tab);
+  var deleteText = document.createTextNode('[x]');
+  var deleteLink = document.createElement('a');
+  deleteLink.setAttribute('href', '#');
+  deleteLink.setAttribute('id', "del_" + tab);
+  deleteLink.appendChild(deleteText);
+  deleteLink.onclick = function () { removeTab(this.getAttribute('id').split('_')[1]); };
+  listItem.appendChild(deleteLink);
+  return listItem;
+}
+
+function buildList(newList) {
+  var ul = document.getElementById('ul');
+  ul.innerHTML = '';
+  for (var i = 0; i < newList.length; i++) {
+    var tab = newList[i];
+    listItem = buildListItem(tab);
+    ul.appendChild(listItem);
+  }
+}
+
+//-------------------------------------
+
+//------- Chrome Local Storage --------
+
+function saveTabs() {
+  var opt = document.getElementById('opt').value;
   chrome.storage.sync.get({
-    myList: []
+    myTabs: []
   }, function(items) {
-    refreshList(items.myList);
+    var tabsFromStorage = items.myTabs;
+    tabsFromStorage.push(opt);
+    chrome.storage.sync.set({
+      myTabs: tabsFromStorage
+    }, function() {
+      buildList(tabsFromStorage);
+      clearTextFieldInput();
+      showStatus('Options saved.');
+    });
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-document.addEventListener('DOMContentLoaded', insertPicker);
-document.getElementById('opt').addEventListener('click', function() {this.select();});
-
-document.onkeydown=function(){
-    if(window.event.keyCode=='13' && document.getElementById('opt').value != ""){
-        save_options();
-    }
+function restoreTabs() {
+  chrome.storage.sync.get({
+    myTabs: []
+  }, function(items) {
+    buildList(items.myTabs);
+  });
 }
+
+//-------------------------------------
+
+initializePage();
 
 
--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,5 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
@@ -17,7 +7,6 @@ function getCurrentTabUrl(callback) {
 
   chrome.tabs.query(queryInfo, function(tabs) {
     var tab = tabs[0];
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
     console.assert(typeof url == 'string', 'tab.url should be a string');
     callback(url);
